feb987238bdc4fb5dae80a0fb855af4b4f7d2c58
960830 <8451266+J--Y@user.noreply.gitee.com>
2021-01-18 16:51:24 +0800

    new提交

---
+++ src/router/Gcy.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        path: '/searchZ',
+        name: 'searchZ',
+        component: () => import('../views/Gcy/searchZ.vue')
+    },
+    {
+        path: '/xaingq',
+        name: 'xaingq',
+        component: () => import('../views/Gcy/xaingq.vue')
+    },
+]
