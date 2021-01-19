57a02abc19c9147972fa6c0f262f4246b585bc2d
邓凯元 <8181397+D-kaiyuan@user.noreply.gitee.com>
2021-01-19 19:35:09 +0800

    邓凯元

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,20 @@
 </template>
 
 <style lang="scss">
+// <<<<<<< HEAD
+// * {
+// =======
 *{
+// >>>>>>> origin/dky
   margin: 0;
   padding: 0;
+  font-size: 0.3rem;
 }
+// <<<<<<< HEAD
 html {
   font-size: 100px;
 }
+// =======
 ol,ul{
   list-style: none;
 }
@@ -23,4 +30,6 @@ i{
     width: 100%;
     height: 100%;
 }
+
+// >>>>>>> origin/dky
 </style>
